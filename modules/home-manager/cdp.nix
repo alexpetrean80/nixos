c4964031949bf791c3c57e62a6c2b72f694a36e2
Alex Petrean <alex_petrean@icloud.com>
2024-02-26 23:16:51 +0200

    remove tmux conf

--- modules/home-manager/cdp.nix
+++ modules/home-manager/cdp.nix
@@ -1,8 +1,20 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-  xdg.configFile.cdp = {
-    recursive = true;
-    source = ../../config/cdp;
+  xdg.configFile.cdp."config.yaml" = {
+    text = ''
+      editor: nvim
+      multiplexer: tmux
+
+      source:
+        # Directories containing the project in your HOME
+        dirs:
+          - Repos
+        # Markers to identify the project root
+        project_markers:
+          - .git
+          - node_modules
+          - go.mod
+    '';
   };
 }
