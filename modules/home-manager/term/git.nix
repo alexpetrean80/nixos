a5cac91e63eb24e2a64849e1d7cee33536f9b134
Alex Petrean <alex_petrean@icloud.com>
2024-02-29 10:08:34 +0200

    update git config

--- modules/home-manager/term/git.nix
+++ modules/home-manager/term/git.nix
@@ -3,5 +3,33 @@
     enable = true;
     userName = "Alex Petrean";
     userEmail = "alex_petrean@icloud.com";
+
+    diff-so-fancy.enable = true;
+    signing = {
+      key = null;
+      signByDefault = true;
+    };
+    ignores = [
+      "node_modules"
+      ".DS_Store"
+      ".AppleDouble"
+      ".LSOverride"
+      "._*" # thumbnails
+      # vim related
+      "[._]*.s[a-v][a-z]"
+      "!*.svg"
+      "[._]*.sw[a-p]"
+      "[._]s[a-rt-v][a-z]"
+      "[._]ss[a-gi-z]"
+      "[._]sw[a-p]"
+      "[._]*.un~" # persistent undo
+    ];
+    extraConfig = {
+      pull.rebase = true;
+      init.defaultBranch = "main";
+      "url \"ssh://git@github.com/\"" = {
+        insteadOf = "https://github.com/";
+      };
+    };
   };
 }
