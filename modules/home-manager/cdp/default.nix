79f3ae421226b9f4d0b1be16a9b297fbf708af0f
Alex-Tudor Petrean <alex@padfoot.cc>
2024-03-28 01:44:55 +0200

    update cdp version

--- modules/home-manager/cdp/default.nix
+++ modules/home-manager/cdp/default.nix
@@ -11,10 +11,13 @@
       owner = "alexpetrean80";
       repo = "cdp";
       rev = "main";
-      hash = "sha256-OBSci+cuDJ8TshsKWao/cRbM70iIc7Nu4RpLXFygOwg=";
+      hash = "sha256-/WMEPwN1DQGN+Y3lJOiUiPV0LKZZbiWJ4ZJXYZ8NmXw=";
     };
+      # keep this because when updating CDP we need a new vendor hash and 
+      # it can only be found by specifying one that is completely wrong (not one from a prev derivation)
+      # vendorHash = "sha256-/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      vendorHash = "sha256-0kkZdotONr28lPCCZrX/O3GunKEXUXWaSIF77EjgbGc=";
 
-    vendorHash = "sha256-fxP9qgEPHrjCwSxy5SUSeYtEln3zXXBHBztLaOz9U3k=";
 
     meta = with lib; {
       homepage = "https://github.com/alexpetrean80/cdp";
